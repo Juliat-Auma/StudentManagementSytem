9a140d7646fd0140476e38db17d2997505c111a2
Juliat Auma <lewiskyadhiambo@gmail.com>
2025-04-06 05:44:07 +0300

    Final update: completed SRMS project

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+
+<?php
+
+error_reporting(0);
+
+session_start();
+
+session_destroy();
+
+if ($_SESSION['message']) {
+
+$message = $_SESSION['message'];
+
+echo "<script type='text/javascript'>
+
+alert('$message');
+
+ </script>";
+
+}
+
+$host = "localhost";
+
+$user = "root";
+
+$password = "";
+
+$db = "schoolproject";
+
+$data = mysqli_connect($host,$user,$password,$db);
+
+$sql = "SELECT * FROM teacher";
+
+$result = mysqli_query($data,$sql);
+
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -29,7 +68,7 @@
 
     <li><a href="">Admission</a></li>
 
-    <li><a href="" class="btn btn-primary">Login</a></li>
+    <li><a href="login.php" class="btn btn-primary">Login</a></li>
 
 </ul>
 
@@ -42,35 +81,77 @@
 </div>
 
 <center>
+    
+    <div>
+        <h1>Our Teachers</h1>
+
+        <div class="container">
+            
+            <div class="row">
+
+                <?php
+
+                while($info=$result->fetch_assoc())
+                    
+                    {
+
+                ?>
+                
+                <div class="col-md-4">
+
+                    <img class="teacher" src="<?php echo "{$info['image']}"  ?>">
+
+                    <h3><?php echo "{$info['name']}"  ?></h3>
+
+                    <h5><?php echo "{$info['description']}"  ?></h5>
+
+
+                </div>
+
+                <?php
+
+            }
+
+            ?>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</center>
+
+<center>
     <h1 class="adm">Admission Form</h1>
 </center>
 <div align="center" class="admission_form" >
 
-    <form>
+    <form action="data_check.php" method="POST">
         <div class="adm_int">
             <label class="label_text">Name</label>
-           <input class="input_deg" type="" name="">
+           <input class="input_deg" type="" name="name" placeholder="Enter your Name">
 
         </div>
 
         <div class="adm_int">
             <label class="label_text">Email</label>
-            <input class="input_deg" type="" name="">
+            <input class="input_deg" type="" name="email" placeholder="Enter your Email">
         </div>
 
         <div class="adm_int">
             <label class="label_text">Phone</label>
-            <input class="input_deg" type="" name="">
+            <input class="input_deg" type="" name="phone" placeholder="Enter your Phone Number">
 
         </div>
 
         <div class="adm_int">
             <label class="label_text">Message</label>
-            <textarea class="input_txt"></textarea>
+            <textarea class="input_txt" name="message" placeholder="Type here"></textarea>
         </div>
 
         <div class="adm_int">
-            <input class="btn btn-primary"  id= "submit" type="submit" value="Apply">
+            <input class="btn btn-primary"  id= "submit" type="submit" value="Apply" name="apply">
         </div>
 
 
